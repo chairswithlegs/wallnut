e5b5c948d555a7efbbc09e4c4c7a64e38c532d7a
Chairs <chairswithlegs@users.noreply.github.com>
2018-06-01 15:39:40 -0400

    Extended Express with asyncRender() to support async/await syntax.

---
+++ util/asyncRender.js
@@ -0,0 +1,21 @@
+//This module extends the Express response object with the asyncRender function
+
+//The asyncRender function wraps the application's render function in a promise, 
+//making it async/await compatible.
+
+module.exports = function(expressApp) {
+
+    async function asyncRender (name, options={}) {
+        return new Promise((resolve, reject) => {
+            expressApp.render(name, options, (err, html) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(html);
+                }
+            });
+        });
+    }
+
+    expressApp.response.asyncRender = asyncRender;
+}
