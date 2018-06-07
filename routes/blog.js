dc3ad648af7c5f19b8abd6062981f6530fe3fbec
Chairs <chairswithlegs@users.noreply.github.com>
2018-06-06 22:02:12 -0400

    Updated blog routing for testing purposes.

--- routes/blog.js
+++ routes/blog.js
@@ -5,11 +5,11 @@ module.exports = router;
 
 router.get('/', async(req, res) => {
     //const blogContent = await res.asyncRender('blog');
-    const blogContent = await res.asyncRender(res.viewMap.getView('blog'));
+    const blogContent = await res.view.asyncRender('blog');
 
-    const html = await res.asyncRender('./views/layout', { content: blogContent });
+    const html = await res.view.asyncRender('./views/layout', { content: blogContent }, false);
 
-    res.header({ 'Content-Type': 'text/html' });
+    res.setHeader('Content-Type', 'text/html');
     res.write(html);
     res.end();
 });
