d7f4975cc48c69cd005f6d8ef649084eb33402ba
Chairs <chairswithlegs@users.noreply.github.com>
2018-06-08 09:45:33 -0400

    Some restructing and added async wrapper.

--- server.js
+++ server.js
@@ -1,47 +1,55 @@
-/*
-* MODULES
-*/
-
-require('dotenv').config();
-const express = require('express');
-const configuration = require('./wallnut.json');
-const viewManager = require('./middleware/view-manager');
-const mongoose = require('mongoose');
-
-//Modules with depencies
-const themeManager = require('./services/theme-manager')(configuration);
-const blogRouter = require('./routes/blog')(configuration);
-
-//Additional Setup
-const app = express();
-const db = mongoose.connect(process.env.CONNECTION_STRING);
-
-
-/*
-* APP
-*/
-
-//Template Engine
-app.set('view engine', 'pug');
-app.set('views', __dirname);
-
-//Middleware
-app.use(express.json());
-app.use(viewManager(app, themeManager, configuration));
-themeManager.activateTheme('dev-theme');
-
-//Routes
-app.use('/blog', blogRouter);
-
-//TODO: Abstract base route into its own file
-app.get('/', async (req, res) => {
-    let html1 = await res.asyncRender('post', {postName: 'This is the title', postBody: 'Body here'});
-    res.header({ 'Content-Type': 'text/html' });
-    res.write(html1);
-    res.end();
-});
-
-//Start the server
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server listening on port ${server.address().port}`);
-});
+(async () => { //Wrapper for async/await compatibility
+    
+    /*
+    * MODULES
+    */
+    
+    //Update the environment variables
+    require('dotenv').config();
+    
+    //Load modules here
+    const express = require('express');
+    const configuration = require('./wallnut.json');
+    const viewManager = require('./middleware/view-manager');
+    const mongoose = require('mongoose');
+    
+    //Modules with additional setup
+    const themeManager = require('./services/theme-manager')(configuration);
+    const blogRouter = require('./routes/blog')(configuration);
+    
+    
+    /*
+    * APP
+    */
+    
+    //Create the App
+    const app = express();
+    
+    //Template Engine
+    app.set('view engine', 'pug');
+    app.set('views', __dirname);
+    
+    //Middleware
+    app.use(express.json());
+    app.use(await viewManager(app, themeManager, configuration));
+    themeManager.activateTheme('dev-theme');
+    
+    //Routes
+    app.use('/blog', blogRouter);
+    
+    //TODO: Abstract base route into its own file
+    app.get('/', async (req, res) => {
+        let html1 = await res.view.asyncRender('post', {postName: 'This is the title', postBody: 'Body here'});
+        res.header({ 'Content-Type': 'text/html' });
+        res.write(html1);
+        res.end();
+    });
+    
+    //Connect to the database and, if successful, start the server
+    mongoose.connect(process.env.CONNECTION_STRING).then(() => {
+        const server = app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server listening on port ${server.address().port}`);
+        });
+    })
+    .catch((error) => console.log(error.message));
+})();
