36831bebc5422dcfdcd32573d3bd1149cde99c5f
Chairs <chairswithlegs@users.noreply.github.com>
2018-06-13 15:34:17 -0400

    Updated server.js to abstract service and pipeline setup.

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
-(async () => { //Wrapper for async/await compatibility
-    
-    /*
-    * MODULES
-    */
-    
-    //Update the environment variables
-    require('dotenv').config();
-    
-    //Load modules here
-    const express = require('express');
-    const configuration = require('./wallnut.json');
-    const mongoose = require('mongoose');
-    
-    //Modules with additional setup
-    const app = express();
+//Update the environment variables
+require('dotenv').config();
 
-    const themeManager = require('./services/theme-manager')(configuration);
-    const viewManager = require('./services/view-manager')(app, themeManager, configuration);
-    const blogRouter = require('./routes/blog')(configuration, viewManager);
-    themeManager.activateTheme('dev-theme');
+//Load modules here
+const express = require('express');
+const configuration = require('./wallnut.json');
+const mongoose = require('mongoose');
+const themeManager = require('./services/theme-manager');
+const viewManager = require('./services/view-manager');
+const blogRouter = require('./routes/blog');
+
+//Create the app and configure
+const app = express();
+
+serviceContainer = configureServices(app);
+configureApp(serviceContainer);
+
+//FOR TESTING PURPOSES ONLY
+serviceContainer.themeManager.activateTheme('dev-theme');
+
+//Connect to the database and, if successful, start the server
+mongoose.connect(process.env.CONNECTION_STRING).then(() => {
+    const server = app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server listening on port ${server.address().port}`);
+    });
+})
+.catch((error) => console.log(error.message));
+
+//Creates services and manages dependencies
+function configureServices(app) {
+    const serviceContainer = {}
     
-    /*
-    * APP
-    */
+    serviceContainer.themeManager = require('./services/theme-manager')(configuration);
+    serviceContainer.viewManager = require('./services/view-manager')(app, serviceContainer.themeManager, configuration);
+    serviceContainer.blogRouter = require('./routes/blog')(configuration, serviceContainer.viewManager);
     
-    //Template Engine
+    return serviceContainer;
+}
+
+//Configures the App pipeline
+function configureApp(serviceContainer) {
     app.set('view engine', 'pug');
     app.set('views', __dirname);
-    
-    //Middleware
     app.use(express.json());
-    
-    //Routes
-    app.use('/blog', blogRouter);
-    
-    //Connect to the database and, if successful, start the server
-    mongoose.connect(process.env.CONNECTION_STRING).then(() => {
-        const server = app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server listening on port ${server.address().port}`);
-        });
-    })
-    .catch((error) => console.log(error.message));
-})();
+    app.use('/blog', serviceContainer.blogRouter);
+}
