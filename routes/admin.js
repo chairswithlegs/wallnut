f85e94155fa281e7a631423e5a861df381c6439b
Chairs <chairswithlegs@users.noreply.github.com>
2018-06-17 19:52:56 -0400

    Added post creation functionality.

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = function(configuration, viewManager) {
     //The instance we will be returning (see below)
     const router = express.Router();
-
+    
     //Private
     const User = require(path.resolve(`${configuration.modelsDirectory}/user`));
     const Post = require(path.resolve(`${configuration.modelsDirectory}/post`));
@@ -15,7 +15,7 @@ module.exports = function(configuration, viewManager) {
     router.get('/', async(req, res) => {
         res.send('Welcome Page Here');
     });
-
+    
     router.get('/posts', async(req, res) => {
         try {
             const posts = await new Promise((resolve, reject) => {
@@ -27,16 +27,21 @@ module.exports = function(configuration, viewManager) {
                     }
                 });
             });
-
+            
             html = await viewManager.renderAdminPageAsync('admin-posts', { posts: posts });
             res.header('Content-Type', 'text/html').send(html);
-
+            
         } catch(error) {
             console.log(error);
             res.status(500).send('Server error.');
         }
     });
 
+    router.get('/posts/create', async(req, res) => {
+        html = await viewManager.renderAdminPageAsync('admin-edit-post');
+        res.header('Content-Type', 'text/html').send(html);
+    });
+    
     router.get('/posts/:id', async(req, res) => {
         try {
             const post = await new Promise((resolve, reject) => {
@@ -48,33 +53,81 @@ module.exports = function(configuration, viewManager) {
                     }
                 });
             });
-
+            
             html = await viewManager.renderAdminPageAsync('admin-edit-post', { post: post });
             res.header('Content-Type', 'text/html').send(html);
-
+            
         } catch(error) {
             console.log(error);
             res.status(500).send('Server error.');
         }
     });
-
+    
     router.put('/posts/:id', (req, res) => {
-        console.log(req.body);
-        res.end();
-        //res.send(res.body);
+        try {
+            if (!req.body.title || !req.body.content || !req.body.id) {
+                res.status(400).send('Post must include a title, content, and id.');
+            } else {
+                Post.findById(req.body.id, (error, post) => {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).send('Server error.');
+                    } else if (!post) {
+                        res.status(400).send('Post not found.');
+                    } else {
+                        post.title = req.body.title;
+                        post.content = req.body.content;
+                        post.save((error , updatedPost) => {
+                            if (error) {
+                                res.status(500).send('Server error.');
+                            } else {
+                                res.json(updatedPost);
+                            }
+                        });
+                    }
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Server error.');
+        }
     });
-
+    
     router.post('/posts', (req, res) => {
-        //Create a post
+        try {
+            if (!req.body.title || !req.body.content) {
+                res.status(400).send('Post must include a title and content.');
+            } else {
+                //Populate the new post with the request values
+                let post = {
+                    title: req.body.title,
+                    content: req.body.content,
+                }
+                if (req.body.date) { post.date = req.body.date } //Optionally add date
+                
+                //Attempt to create the post in the database
+                Post.create(post, (error, post) => {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).send('Server error.');
+                    } else {
+                        res.json(post);
+                    }
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Server error.');
+        }
     });
-
+    
     router.get('/settings', (req, res) => {
         //Display the site settings
     });
-
+    
     router.put('/settings', (req, res) => {
         //Update the site settings
     });
-
+    
     return router;
 }
--- views/admin-edit-post.js
+++ views/admin-edit-post.js
@@ -4,17 +4,38 @@ document
 
 function submitForm() {
     var formData = {
-        title: document.getElementById('form-title').value,
-        content: document.getElementById('form-content').value,
-        id: document.getElementById('form-id').value
+        title: $('#form-title').val(),
+        content: $('#form-content').val(),
+        id: $('#form-id').val()
     }
-    
+
+    //If we have no ID, then this is treated as a new post.
+    if (formData.id === "") {
+        postForm(formData);
+    } else {
+        putForm(formData);
+    }
+}
+
+function putForm(formData) {
     var req = new XMLHttpRequest();
     req.open('PUT', './' + formData.id);
     req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    req.send(JSON.stringify({ test: 'test' }));
+    req.send(JSON.stringify(formData));
     req.addEventListener('load', function() {
         submitAlert(req.status === 200);
+    });
+}
+
+function postForm(formData) {
+    var req = new XMLHttpRequest();
+    req.open('POST', './' + formData.id);
+    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    req.send(JSON.stringify(formData));
+    req.addEventListener('load', function() {
+        if (req.status === 200)
+        submitAlert(true);
+        $('#form-id').val(JSON.parse(req.response)._id);
     });
 }
 
@@ -27,4 +48,9 @@ function submitAlert(success) {
     } else {
         $('#error-alert').removeClass('d-none');
     }
+
+    setTimeout(() => {
+        $('#success-alert').addClass('d-none');
+        $('#error-alert').addClass('d-none');
+    }, 3000);
 }
