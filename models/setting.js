8cb8becd348588e992a7213ef680ff6bfff9b954
Chairs <chairswithlegs@users.noreply.github.com>
2018-07-07 14:05:23 -0400

    Added support for generating site setting options based on the active theme.

--- models/setting.js
+++ models/setting.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const settingSchema = mongoose.Schema({
     key: { type: String, required: true, unique: true },
-    value: Object
+    value: Object,
+    themeSetting: { type: Boolean, required: true }
 });
 
 module.exports = mongoose.model('Setting', settingSchema);
--- services/settings-manager.js
+++ services/settings-manager.js
@@ -1,32 +1,33 @@
 //This service is used to manage theme and site settings in wallnut
 
 const path = require('path');
+const Setting = require('../models/setting');
 
-function SettingsManager(settingModel, themeManager) {
+function SettingsManager(themeManager) {
     let siteSettingsCache;
     
-    this.Setting = settingModel;
-
+    this.themeManager = themeManager;
+    
     //Getter for the site setting cache
     this.getSiteSetting = function(setting) {
         return siteSettingsCache[setting]
     }
-
+    
     //Syncs the cache with the database
     this.loadSiteSettings = function() {
         return new Promise((resolve, reject) => {
-            this.Setting.find({}, (error, settings) => {
+            Setting.find({}, (error, settings) => {
                 if (error) {
                     reject(error);
                 } else {
                     siteSettingsCache = {};
-
+                    
                     settings.forEach((setting) => {
                         siteSettingsCache[setting.key] = setting.value;
                     });
-
+                    
                     Object.freeze(siteSettingsCache);
-
+                    
                     resolve(siteSettingsCache);
                 }
             });
@@ -35,33 +36,34 @@ function SettingsManager(settingModel, themeManager) {
 }
 
 //Add or update a site setting in the database
-SettingsManager.prototype.setSiteSetting = function(key, value) {
+SettingsManager.prototype.setSiteSetting = function(key, value, themeSetting=false) {
     return new Promise((resolve, reject) => {
-        this.Setting.findOne({ key: key }, (error, setting) => {
+        Setting.findOne({ key: key }, (error, setting) => {
             if (error) {
                 reject(error);
             } else if (!setting) { //If the setting doesn't exist, create it
-                Setting.create({ key: key, value: value }, (error, setting) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        this.loadSiteSettings(); //Sync the cache
-                        resolve(setting);
-                    }
-                });
-            } else { //If the setting does exist, update it
-                setting.value = value;
-                setting.save((error, updatedSetting) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        this.loadSiteSettings(); //Sync the cache
-                        resolve(updatedSetting);
-                    }
-                });
-            }
-        });
+            Setting.create({ key: key, value: value, themeSetting: themeSetting }, (error, setting) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    this.loadSiteSettings(); //Sync the cache
+                    resolve(setting);
+                }
+            });
+        } else { //If the setting does exist, update it
+            setting.value = value;
+            setting.themeSetting = themeSetting;
+            setting.save((error, updatedSetting) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    this.loadSiteSettings(); //Sync the cache
+                    resolve(updatedSetting);
+                }
+            });
+        }
     });
+});
 }
 
 //Getter for the active theme setting (generated from the theme config)
@@ -69,10 +71,37 @@ SettingsManager.prototype.getActiveThemeSetting = function(setting) {
     return this.themeManager.getActiveThemeSetting(setting);
 }
 
+//Removes any theme settings from the database
+SettingsManager.prototype.clearThemeSettings = function() {
+    return new Promise((resolve, reject) => {
+        Setting.deleteMany({ themeSetting: true }, (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
 //Async factory function to ensure cache is loaded before making the service available
 module.exports = async function(configuration, themeManager) {
-    Setting = require(`${path.resolve(configuration.modelsDirectory)}/setting.js`);
-    settingsManager = new SettingsManager(Setting, themeManager);
+    settingsManager = new SettingsManager(themeManager);
+    
+    themeManager.on('theme-activated', async() => {
+        await settingsManager.clearThemeSettings();
+        siteSettings = settingsManager.getActiveThemeSetting('siteSettings');
+        
+        if (process.env.NODE_ENV) {
+            console.log('Set the following theme settings:');
+        }
+        
+        Object.keys(siteSettings).forEach((key) => {
+            settingsManager.setSiteSetting(key, siteSettings[key], true);
+            console.log(key + ': ' + siteSettings[key]);
+        });
+    });
+    
     await settingsManager.loadSiteSettings();
     return settingsManager;
 }
